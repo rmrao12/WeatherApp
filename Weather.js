2641ac7d4048273968415c366b69b8108f8dce2f
Ghulam-Mustafa-Rao <rao01231@gmail.com>
2024-08-05 16:12:46 +0500

    Second Code Base

--- Weather.js
+++ Weather.js
@@ -1,115 +1,6 @@
-const cities = [
-    { 
-        location: "Rawalpindi", 
-        country:"Pakistan",
-        forecast: {
-            sunday: { temperature: "30°C", weather: "Sunny" },
-            monday: { temperature: "31°C", weather: "Cloudy" },
-            tuesday: { temperature: "34°C", weather: "Rainy" },
-            wednesday: { temperature: "33°C", weather: "Sunny" },
-            thursday: { temperature: "32°C", weather: "Cloudy" },
-            friday: { temperature: "34°C", weather: "Sunny" },
-            saturday: { temperature: "35°C", weather: "Rainy" }
-        }
-    },
-    { 
-        location: "Islamabad", 
-        country:"Pakistan",
-        forecast: {
-            sunday: { temperature: "29°C", weather: "Cloudy" },
-            monday: { temperature: "30°C", weather: "Sunny" },
-            tuesday: { temperature: "32°C", weather: "Rainy" },
-            wednesday: { temperature: "31°C", weather: "Sunny" },
-            thursday: { temperature: "30°C", weather: "Cloudy" },
-            friday: { temperature: "32°C", weather: "Sunny" },
-            saturday: { temperature: "33°C", weather: "Rainy" }
-        }
-    },
-    { 
-        location: "Karachi", 
-        country:"Pakistan",
-        forecast: {
-            sunday: { temperature: "34°C", weather: "Sunny" },
-            monday: { temperature: "35°C", weather: "Cloudy" },
-            tuesday: { temperature: "36°C", weather: "Sunny" },
-            wednesday: { temperature: "36°C", weather: "Cloudy" },
-            thursday: { temperature: "35°C", weather: "Sunny" },
-            friday: { temperature: "36°C", weather: "Rainy" },
-            saturday: { temperature: "37°C", weather: "Sunny" }
-        }
-    },
-    { 
-        location: "Lahore", 
-        country:"Pakistan",
-        forecast: {
-            sunday: { temperature: "33°C", weather: "Sunny" },
-            monday: { temperature: "34°C", weather: "Cloudy" },
-            tuesday: { temperature: "35°C", weather: "Rainy" },
-            wednesday: { temperature: "34°C", weather: "Sunny" },
-            thursday: { temperature: "33°C", weather: "Cloudy" },
-            friday: { temperature: "35°C", weather: "Sunny" },
-            saturday: { temperature: "36°C", weather: "Rainy" }
-        }
-    },
-    { 
-        location: "Multan", 
-        country:"Pakistan",
-        forecast: {
-            sunday: { temperature: "37°C", weather: "Sunny" },
-            monday: { temperature: "38°C", weather: "Cloudy" },
-            tuesday: { temperature: "39°C", weather: "Sunny" },
-            wednesday: { temperature: "38°C", weather: "Cloudy" },
-            thursday: { temperature: "37°C", weather: "Sunny" },
-            friday: { temperature: "38°C", weather: "Rainy" },
-            saturday: { temperature: "39°C", weather: "Sunny" }
-        }
-    },
-    { 
-        location: "Peshawar", 
-        country:"Pakistan",
-        forecast: {
-            sunday: { temperature: "33°C", weather: "Cloudy" },
-            monday: { temperature: "34°C", weather: "Sunny" },
-            tuesday: { temperature: "34°C", weather: "Rainy" },
-            wednesday: { temperature: "33°C", weather: "Cloudy" },
-            thursday: { temperature: "32°C", weather: "Sunny" },
-            friday: { temperature: "34°C", weather: "Rainy" },
-            saturday: { temperature: "35°C", weather: "Sunny" }
-        }
-    },
-    { 
-        location: "Swat", 
-        country:"Pakistan",
-        forecast: {
-            sunday: { temperature: "27°C", weather: "Rainy" },
-            monday: { temperature: "28°C", weather: "Cloudy" },
-            tuesday: { temperature: "28°C", weather: "Sunny" },
-            wednesday: { temperature: "27°C", weather: "Rainy" },
-            thursday: { temperature: "26°C", weather: "Cloudy" },
-            friday: { temperature: "28°C", weather: "Sunny" },
-            saturday: { temperature: "29°C", weather: "Rainy" }
-        }
-    },
-    { 
-        location: "Sydney", 
-        country:"Australia",
-        forecast: {
-            sunday: { temperature: "29°C", weather: "Cloudy" },
-            monday: { temperature: "30°C", weather: "Sunny" },
-            tuesday: { temperature: "32°C", weather: "Rainy" },
-            wednesday: { temperature: "31°C", weather: "Sunny" },
-            thursday: { temperature: "30°C", weather: "Cloudy" },
-            friday: { temperature: "32°C", weather: "Sunny" },
-            saturday: { temperature: "33°C", weather: "Rainy" }
-        }
-    },
-];
-const today = new Date();
 
-// Get the day of the week as a number (0 for Sunday, 1 for Monday, etc.)
-const dayNumber = today.getDay();
-const daysOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-const currentDay = daysOfWeek[dayNumber];
+
+const apiKey = '78f52716c353bfe1231fc47d44fd6ff9'; 
 const searchBar = document.getElementById('searchBar');
 const suggestions = document.getElementById('suggestions');
 const selectedLocation = document.getElementById('selected-location');
@@ -117,80 +8,143 @@ const selectedCountry = document.getElementById('selected-country');
 const temperature = document.getElementById('temperature');
 const weatherIcon = document.getElementById('weathericon');
 const weeklyForecastContainer = document.getElementById('weeklyForecast');
-
+const hourlyForecastContainer = document.getElementById('hourlyForecastContainer');
 // Event listener for input changes
-searchBar.addEventListener('input', function() {
-    const input = searchBar.value.toLowerCase();
+searchBar.addEventListener('input', async function() {
+    const input = searchBar.value.trim();
     suggestions.innerHTML = ''; 
 
-    if (input) {
-        
-        const filteredPlaces=[];
-    
-        
-        for (let i = 0; i < cities.length; i++) {
-            
-            const place = cities[i];
-           
-            if (place.location.toLowerCase().includes(input)) 
-            {
-                filteredPlaces.push(place);
-            }
-        }
-      
-       // const filteredPlaces = cities.filter(place => place.location.toLowerCase().includes(input));   //above work can also be done through this
-        console.log(filteredPlaces)
-        filteredPlaces.forEach(place => {
-            const suggestionItem = document.createElement('div');
-            suggestionItem.textContent = place.location;
-            suggestionItem.addEventListener('click', function() {
-                searchBar.value = place.location;
-                document.getElementById('showTemperature').style.display='block';
-                weeklyForecastContainer.style.display='flex';
-              selectedLocation.innerHTML = place.location;
-              selectedCountry.innerHTML = place.country;
-              temperature.innerHTML=place.forecast[currentDay].temperature;
-              const weather=place.forecast[currentDay].weather;
-              console.log(weather)
-              suggestions.innerHTML = ''; 
-              searchBar.value='';
-                ShowWeatherIcon(weather);
-                displayWeeklyForecast(place.forecast);
+    if (input.length > 1) { // Start searching after 3 characters
+        try {
+            const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=5&appid=${apiKey}`);
+            const data = await response.json();
 
+            console.log(data)
+            data.forEach(place => {
+                const suggestionItem = document.createElement('div');
+                suggestionItem.classList.add('suggestion-item');
+                suggestionItem.textContent = `${place.name}, ${place.country}`;
+                suggestionItem.addEventListener('click', function() {
+                    searchBar.value = place.name;
+                    document.getElementById('showTemperature').style.display = 'block';
+                    selectedLocation.innerHTML = place.name;
+                    selectedCountry.innerHTML = place.country;
+                    fetchWeatherData(place.lat, place.lon,input);
+                    suggestions.innerHTML = ''; 
+                    searchBar.value = '';
+                });
+                suggestions.appendChild(suggestionItem);
             });
-            suggestions.appendChild(suggestionItem);
-        });
+        } catch (error) {
+            console.error("Error fetching suggestions:", error);
+        }
     }
 });
 
 
-//to display weekly weather starting from current day to end
-function displayWeeklyForecast(forecast) {
-    
-    weeklyForecastContainer.innerHTML = ''; // Clear previous forecast
 
-    daysOfWeek.forEach((day, index) => {
-        const dayCard = document.createElement('div');
-       
-        dayCard.innerHTML = `
-            <p>${day.charAt(0).toUpperCase() + day.slice(1)}</p>
-            <div>${forecast[day].temperature}</div>
-            <div>${getWeatherIcon(forecast[day].weather)}</div>
-        `;
-        weeklyForecastContainer.appendChild(dayCard);
-    });
+async function fetchWeatherData(lat, lon ,loc) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
+        const weatherData = await response.json();
+
+        const currentWeather = weatherData.weather[0].main;
+        const currentTemp = weatherData.main.temp + '°C';
+        weeklyForecastContainer.style.display='flex';
+        hourlyForecastContainer.style.display='flex';
+        temperature.innerHTML = currentTemp;
+        ShowWeatherIcon(currentWeather);
+       getWeatherData(loc)
+      
+    
+     
+
+    } catch (error)
+     {
+        console.error("Error fetching weather data:", error);
+        alert("Unable to fetch weather data. Please try again later.");
+    }
+}
+
+function getWeatherData(loc) {
+    
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${loc}&units=metric&appid=${apiKey}`; //used 5 days data key becuase 7 days api key was not free 
+
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Data not Found');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+          fetchWeeklyForecast(data);
+          fetchHourlyForecast(data);
+          
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+          
+        });
 }
 
 
+function fetchWeeklyForecast(data) {
+    
+    
+    weeklyForecastContainer.innerHTML = ''; // Clear previous forecast
+
+    for (let i = 0; i < 5; i++) {
+        const forecastDay = data.list[i * 8];
+        const date = new Date(forecastDay.dt_txt).toLocaleDateString(undefined, { weekday: 'long' });
+        const temp = forecastDay.main.temp;
+       
+
+        const forecastElement = document.createElement('div');
+        forecastElement.classList.add('forecast-day');
+        forecastElement.innerHTML = `
+            <p>${date}</p>
+            <div>${getWeatherIcon(forecastDay.weather[0].main)}</div>
+            <p>${temp}°C</p>
+        `;
+
+        weeklyForecastContainer.appendChild(forecastElement);
+    }
+}
+
+function fetchHourlyForecast(data) {
+   
+    hourlyForecastContainer.innerHTML = ''; // Clear previous forecast
+
+    // Display hourly data for the next 24 hours
+    data.list.slice(0, 8).forEach(hour => {
+        const time = new Date(hour.dt_txt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        const temp = hour.main.temp;
+        const icon = `https://openweathermap.org/img/wn/${hour.weather[0].icon}@2x.png`;
+        const description = hour.weather[0].description;
+
+        const hourElement = document.createElement('div');
+        
+        hourElement.innerHTML = `
+            <p>${time}</p>
+           <img src="${icon}" alt="${description}" style="width:83px; height:83px;">
+            <p>${temp}°C</p>
+            <p>${description}</p>
+        `;
+
+        hourlyForecastContainer.appendChild(hourElement);
+    });
+}
 // simple icon display in daily panel
 function getWeatherIcon(weather) {
     switch (weather) {
-        case "Cloudy":
-            return '<i class="fa fa-cloud fa-lg"></i>';
-        case "Rainy":
-            return '<img src="./Images/rainy.png" alt="Rainy Icon" style="width: 24px;"/>';
-        case "Sunny":
-            return '<i class="fa fa-sun-o fa-lg"></i>';
+        case "Clouds":
+            return '<i class="fa fa-cloud fa-3x" style="margin-top:10px; margin-bottom:14px;"></i>';
+        case "Rain":
+            return '<img src="./Images/rainy.png" alt="Rainy Icon" style="width: 54px; margin-top:10px;  margin-bottom:10px;"/>';
+        case "Clear":
+            return '<i class="fa fa-sun-o fa-3x" style="margin-top:10px; margin-bottom:14px;"></i>';
     }
 }
 
@@ -198,10 +152,17 @@ function getWeatherIcon(weather) {
 //show icon in today weather
 function ShowWeatherIcon(weather)
 {
+    console.log(weather)
     let iconHtml
     switch (weather)
     {
-        case "Cloudy":
+        case "Mist":
+            iconHtml =  '<i class="fa fa-cloud fa-lg" style="color: white;"></i>';
+            
+            // Insert the HTML into the container
+            weatherIcon.innerHTML = iconHtml;
+            break;
+        case "Clouds":
             iconHtml =  '<i class="fa fa-cloud fa-lg" style="color: white;"></i>';
             
             // Insert the HTML into the container
@@ -211,7 +172,7 @@ function ShowWeatherIcon(weather)
             iconHtml =  '<img src="./Images/rainy.png" style="color:white;"/>';
             weatherIcon.innerHTML = iconHtml;
            break;
-        case "Sunny":
+        case "Clear":
             iconHtml =  '<i class="fa fa-sun-o fa-lg" style="color: white;"></i>';
             weatherIcon.innerHTML = iconHtml;
            break;
